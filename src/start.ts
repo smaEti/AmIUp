b6a94012c8880810ae7976b68c38a0d084f86bd5
Amin Ehterami <A.Ehterami@proton.me>
2024-08-04 17:14:19 +0330

    feat : start and stop processFiles

--- src/start.ts
+++ src/start.ts
@@ -2,51 +2,62 @@ import { exit } from "process";
 import ps from "ps-node";
 import { spawn } from "child_process";
 import fs from "node:fs";
-export default function startFunction(){
-
-const out = fs.openSync(__dirname + "/out.log", "a+");
-const err = fs.openSync(__dirname + "/err.log", "a+");
-function checkRunning() {
-  return new Promise<void>((resolve) => {
-    ps.lookup(
-      {
-        command: "node",
-      },
-      function (err, resultList) {
-        if (err) {
-          throw new Error(err.message);
-        }
-
-        resultList.forEach(function (process) {
-          if (process) {
-            console.log(
-              "process : ",
-              process.pid,
-              process.arguments,
-              process.command
-            );
-            const processNames = process.arguments.map((item) =>
-              item.split("/")
-            )[0];
-            if (processNames[processNames.length - 1] == "monitorProcess.js") {
-              console.log(
-                "monitorProcess is already running in the background.",
-                process.pid
-              );
-              exit(0);
-            }
+import chalk from "chalk";
+export default function startFunction() {
+  if (!fs.existsSync(__dirname + "/logs")) {
+    fs.mkdirSync(__dirname + "/logs", { recursive: true });
+  }
+  const date = new Date();
+  const out = fs.openSync(
+    __dirname +
+      `/logs/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}-out.log`,
+    "a+"
+  );
+  const err = fs.openSync(
+    __dirname +
+      `/logs/${date.getDate()}-${date.getMonth()}-${date.getFullYear()}-error.log`,
+    "a+"
+  );
+  function checkRunning() {
+    return new Promise<void>((resolve) => {
+      ps.lookup(
+        {
+          command: "node",
+        },
+        function (err, resultList) {
+          if (err) {
+            throw new Error(err.message);
           }
-        });
-        resolve();
-      }
-    );
+
+          resultList.forEach(function (process) {
+            if (process) {
+              const processNames = process.arguments.map((item) =>
+                item.split("/")
+              )[0];
+              if (
+                processNames[processNames.length - 1] == "monitorProcess.js"
+              ) {
+                console.log(
+                  chalk.yellow(
+                    "monitorProcess is already running in the background.",
+                    process.pid
+                  )
+                );
+                exit(0);
+              }
+            }
+          });
+          resolve();
+        }
+      );
+    });
+  }
+  checkRunning().then(() => {
+    const child = spawn("node", [__dirname + "/monitorProcess.js"], {
+      detached: true,
+      stdio: ["ignore", out, err],
+    });
+    child.unref();
+    console.log(chalk.green("App started!"));
   });
-}
-checkRunning().then(() => {
-  const child = spawn("node", [__dirname + "/monitorProcess.js"], {
-    detached: true,
-    stdio: ["ignore", out, err],
-  });
-  child.unref();
-});
 }
--- src/stop.ts
+++ src/stop.ts
@@ -1,35 +1,26 @@
+import chalk from "chalk";
 import ps, { kill } from "ps-node";
-export default function StopFunction(){
-
-    ps.lookup(
-        {
-            command: "node",
-        },
-        function (err, resultList) {
-            if (err) {
+export default function StopFunction() {
+  ps.lookup(
+    {
+      command: "node",
+    },
+    function (err, resultList) {
+      if (err) {
         throw new Error(err.message);
       }
-      
+
       resultList.forEach(function (process) {
-          if (process) {
-              console.log(
-            "process : ",
-            process.pid,
-            process.arguments,
-            process.command
-        );
-        const processNames = process.arguments.map((item) =>
+        if (process) {
+          const processNames = process.arguments.map((item) =>
             item.split("/")
-    )[0];
-    if (processNames[processNames.length - 1] == "monitorProcess.js") {
+          )[0];
+          if (processNames[processNames.length - 1] == "monitorProcess.js") {
             kill(process.pid);
-            console.log(
-                "monitorProcess ended",
-                process.pid
-            );
+            console.log(chalk.green("monitorProcess ended", process.pid));
+          }
         }
-    }
-});
+      });
     }
   );
 }
